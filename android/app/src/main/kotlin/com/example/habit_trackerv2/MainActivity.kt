517c3f76857c24c2995d181463fc63a8bd30f845
AntonKochurka <antonkotchurka@gmail.com>
2025-10-11 14:35:45 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/habit_trackerv2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.habit_trackerv2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:habit_trackerv2/app.dart';
+
+void main() {
+  runApp(const Application());
+}
